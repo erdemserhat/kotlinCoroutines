5ee2c5a04f9069a065a9b75585f84a1251f9f72a
Serhat <me.serhaterdem@gmail.com>
2024-01-09 21:43:02 +0300

    coroutines basics

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,83 +1,64 @@
 package org.example
 
-import kotlinx.coroutines.*
-import java.io.File
-import javax.sound.sampled.AudioSystem
-
-suspend fun playBeats(beats: String, file: String) {
-    var parts = beats.split("x")
-    var count = 0
-
-    for (part in parts) {
-        count += part.length + 1
-        if (part == "") {
-            playSound(file)
-        } else {
-            delay(100 * (part.length + 1L))
-            if (count < beats.length) {
-                playSound(file)
-            }
-        }
-    }
-}
-
-suspend fun writeByPing(message: String) {
-    for (i in 0..5) {
-        println(message)
-        delay(600)
-    }
-}
-
-fun writeByPingThread(message: String) {
-    for (i in 0..5) {
-        println(message)
-        Thread.sleep(600)
-    }
-}
-
-suspend fun playSound(file: String) {
-    try {
-        val audioInputStream = AudioSystem.getAudioInputStream(File(file))
-        val clip = AudioSystem.getClip()
-        withContext(Dispatchers.IO) {
-            clip.open(audioInputStream)
-        }
-        clip.start()
-        // Sleep to ensure the sound finishes playing before moving to the next
-        delay(clip.microsecondLength / 2000)
-        clip.close() // Close the clip to release resources
-        withContext(Dispatchers.IO) {
-            audioInputStream.close()
-        } // Close the audio input stream
-    } catch (e: Exception) {
-        println("Error playing sound: ${e.message}")
-    }
-}
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.isActive
+import kotlinx.coroutines.launch
 
 
 fun main() {
 
 
-    val myThread = Thread(Runnable {
-        // İş parçacığında yapılacak işlemler
-        for (i in 1..5) {
-            println("Thread işlemi: $i")
-            Thread.sleep(212) // Belirli bir süre uyuma işlemi
+    var job =GlobalScope.launch {
+        for (i in 40..50){
+            if (isActive){
+                println(fibonacci(i))
+
+            }
+
         }
-    })
+    }
 
-    val myThread2 = Thread(Runnable {
-        // İş parçacığında yapılacak işlemler
-        for (i in 1..5) {
-            println("Thread işlemi2: $i")
-            Thread.sleep(1000) // Belirli bir süre uyuma işlemi
-        }
-    })
+   GlobalScope.launch {
+       launch {
+           println("1. scope started")
+           job.join()
+           println("1. scope finished")
+       }
 
-    myThread.start()
-    myThread2.start()
 
-    MyDataType("12") sum "122"
+       launch {
+           println("2. scope started")
+           delay(2000)
+           job.cancel()
+           println("2. scope finished")
+       }
+   }
 
+
+
+    Thread.sleep(100000)
+
+
+
+}
+
+
+/**
+ * This function returns the parameterized index of
+ * fibonacci array.
+ * 1,1,2,3,5,8...
+ * Example ; fibonacci(4) = 3
+ * When you call fibonacci(4) ;
+ * fibonacci(4)= fibonacci(3) + fibonacci(2) *
+ * fibonacci(3)= fibonacci(2) + fibonacci(1) = fibonacci(1) +fibonacci(0) + fibonacci(1) = 1+0+1=2
+ * fibonacci(2)= fibonacci(1) + fibonacci(0) = 1+0=1
+ * Therefore, fibonacci(4)= 1+2 = 3
+ *
+ */
+fun fibonacci(n:Int):Int{
+    if(n==0) return 0
+    else if(n==1) return 1
+    return  fibonacci(n-1)+fibonacci(n-2)
 
 }
