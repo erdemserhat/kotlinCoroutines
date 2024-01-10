12813e6a075466c6faa2f16232f4da221b5d8bb0
Serhat <me.serhaterdem@gmail.com>
2024-01-10 04:35:54 +0300

    coroutines basics

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,47 +1,132 @@
 package org.example
 
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.isActive
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
+import kotlin.time.measureTime
 
 
 fun main() {
 
+    /*
 
-    var job =GlobalScope.launch {
-        for (i in 40..50){
-            if (isActive){
+
+        var job = GlobalScope.launch(Dispatchers.Default) {
+            println("Started Job 1")
+            for (i in 40..50) {
                 println(fibonacci(i))
+            }
+        }
 
+
+
+
+
+
+        var job2 = GlobalScope.launch(Dispatchers.Default) {
+            println("Started Job 2")
+            for (i in 40..50) {
+                if (isActive)
+                    println(fibonacci(i))
             }
 
         }
+
+
+
+
+        var job3 = GlobalScope.launch(Dispatchers.Default) {
+            println("Started Job3")
+            withTimeout(2000) {
+                for (i in 40..50) {
+                    if (isActive) {
+                        println(fibonacci(i))
+                    }
+
+                }
+            }
+        }
+
+
+
+        //Let's say we want to cancel "job" after 2000 ms delay
+        //This code won't work properly because job is too busy to call cancel() function inside it.
+        //So we need to add control block examine job2
+        //you can to same operation in the context of job examine job 3
+
+
+        runBlocking {
+            delay(2000)
+            job2.cancel()
+           }
+
+
+
+        //Coroutine
+         var job4 = GlobalScope.launch(Dispatchers.Default) {
+             repeat(5){
+                 println("Coroutine is still working...")
+                 delay(1000)
+             }
+         }
+        //Main thread
+        runBlocking {
+            job4.join()
+        }
+
+
+
+
+
+        val job6 = GlobalScope.launch(Dispatchers.Default) {
+            for (i in 40..50){
+                println(fibonacci(i))
+                delay(1L)
+            }
+        }
+
+        runBlocking {
+            delay(2000)
+            job6.cancel()
+            println("cancelled")
+
+        }
+
+        Thread.sleep(100000)
+
+
+
+
+        }
+
+     */
+
+    GlobalScope.launch(Dispatchers.IO) {
+        val time = measureTime {
+            var answer = async { myAwesomeNetworkCall() }
+            var answer2 = async { myAwesomeNetworkCall2() }
+            println(answer.await())
+            println(answer2.await())
+        }
+        println("Time : $time")
+
     }
 
-   GlobalScope.launch {
-       launch {
-           println("1. scope started")
-           job.join()
-           println("1. scope finished")
-       }
+    GlobalScope.launch(Dispatchers.Default) {
+        var time = measureTime {
+            var myFibonacciAnswer = async {
 
+                fibonacci(40)
+            }
 
-       launch {
-           println("2. scope started")
-           delay(2000)
-           job.cancel()
-           println("2. scope finished")
-       }
-   }
+            println(myFibonacciAnswer.await())
 
+        }
+        println(time)
 
+    }
 
-    Thread.sleep(100000)
-
-
-
+    Thread.sleep(10000)
 }
+
 
 
 /**
@@ -56,9 +141,19 @@ fun main() {
  * Therefore, fibonacci(4)= 1+2 = 3
  *
  */
-fun fibonacci(n:Int):Int{
-    if(n==0) return 0
-    else if(n==1) return 1
-    return  fibonacci(n-1)+fibonacci(n-2)
+fun fibonacci(n: Int): Int {
+    if (n == 0) return 0
+    else if (n == 1) return 1
+    return fibonacci(n - 1) + fibonacci(n - 2)
 
+}
+
+suspend fun myAwesomeNetworkCall(): String {
+    delay(3000)
+    return "answer"
+}
+
+suspend fun myAwesomeNetworkCall2(): String {
+    delay(3000)
+    return "answer"
 }
